510daff0bf78ae83de5b39582c21482db56b94f2
Weiguiyun Yang <n10022481@qut.edu.au>
2019-06-15 23:22:36 +1000

    finish routes

---
+++ src/routes/category.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {
+  getAllCategories,
+  addCategory,
+  updateCategory,
+  deleteCategory,
+  getCategory
+} = require('../controllers/customer');
+
+const {
+  addBusiness,
+  deleteBusiness
+}=require('../controllers/business')
+const router = express.Router();
+
+router.get('/', getAllCategories);
+router.get('/:categoryid', getCategory);
+router.post('/', addCategory);
+router.put('/:categoryid', updateCategory);
+router.delete('/:categoryid', deleteCategory);
+router.post('/:categoryid/businesses/:businessid', addBusiness);
+router.delete('/:categoryid/businesses/:businessid', deleteBusiness);
+
+module.exports = router;
--- src/routes/order.js
+++ src/routes/order.js
@@ -5,8 +5,6 @@ const {
   updateOrder,
   deleteOrder,
   getOrder,
-  addCategory,
-  deleteCatgory
 } = require('../controllers/order');
 const router = express.Router();
 
@@ -15,7 +13,5 @@ router.get('/:orderid', getOrder);
 router.post('/', addOrder);
 router.put('/:orderid', updateOrder);
 router.delete('/:orderid', deleteOrder);
-router.post('/:customerid/orders/:orderid', addCategory);
-router.delete('/:customerid/orders/:orderid', deleteCatgory);
 
 module.exports = router;
