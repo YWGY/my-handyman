fc7dd69db5e62442b44fa173160410dc91299e02
Weiguiyun Yang <n10022481@qut.edu.au>
2019-06-15 23:52:12 +1000

    add morgan package

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,11 +1,22 @@
 const Order = require('../models/order'); 
 
 async function addOrder(req, res){
-    const {orderid} = req.params;
     const {customerName, businessName, status, grade, comment} = req.body;
-
+    
     const existingOrder = await Order.findById(orderid).exec();
 
     if (existingOrder){
         return res.status(400).json('Duplicate order id');
     }
+
+    const order = new Order({
+        customerName, businessName, status, grade, comment
+    });
+    await order.save();
+    return res.json(order);
+}
+
+async function getAllOrders(req, res){
+    const orders = await Order.find().exec();
+    return res.json(orders);
+}
