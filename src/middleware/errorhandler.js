f3ab3d5d85d55adc8d13430deae75bacb23f43bd
weiguiyun yang <39477434+YWGY@users.noreply.github.com>
2019-06-25 15:36:37 +1000

    Update errorhandler.js

--- src/middleware/errorhandler.js
+++ src/middleware/errorhandler.js
@@ -3,5 +3,6 @@ module.exports = (err,req,res,next)=>{
         return res.status(400).json.err()
     }
     console.error(err);
-    return 
+    return res.status(500).json('something unexpected happened');
+    //报错500说明在开发过程中没有抓到错误
 }
