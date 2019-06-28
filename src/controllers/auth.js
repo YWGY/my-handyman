9d976eebc0a79d74c78699be4199f09b5d9a193c
Weiguiyun Yang <n10022481@qut.edu.au>
2019-06-28 15:39:36 +1000

    1

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,19 +2,21 @@ const User = require('../models/user');
 const { generateToken } = require('../utils/jwt');
 
 async function loginUser(req, res) {
-    const { username, password } = req.body;
-  
-    const existingUser = await User.findOne({ username })
-    .exec();
-    
-    if (existingUser.password !== password) {
-      return res.status(400).json('Invalid username or password');
-    }
-  
-    const token = generateToken(existingUser._id);
-    return res.json({ username, token }); 
+  const { username, password } = req.body;
+
+  const existingUser = await User.findOne({ username }).exec();
+  if (!existingUser) {
+    return res.status(401).json('Invalid username or password');
   }
-  
-  module.exports = {
-    loginUser
-  };
+  const validPassword = await existingUser.validatePassword(password);
+  if (!validPassword) {
+    return res.status(401).json('Invalid username or password');
+  }
+
+  const token = generateToken(existingUser._id);
+  return res.json({ username, token });
+}
+
+module.exports = {
+  loginUser
+};
